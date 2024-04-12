eba385cead09070432c9a4b4c0b966b784a348de
Mohammedadn <mohammedaden023@gmail.com>
2024-04-12 12:24:19 +0300

    firsst

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+const cars = document.getElementById("cars");
+
 fetch('http://localhost:3000/vehicles')
   .then(response => response.json())
   .then(data => {
-    console.log(data);
-
-    console.log('Fetched data:', data);
+  data.forEach(vehicle =>{
+    const div = document.createElement("div");
+    div.className = "automade";
+    div.innerHTML = `
+    <img src="${vehicle.photo}"/>
+     <p> ${vehicle.make}   ${vehicle.model}</p>
+     <p>year: ${vehicle.year}</p>
+     <p>color: ${vehicle.color}</p>
+     <p>price: ${vehicle.price}</p>
+     <p>mileage: ${vehicle.mileage}</p>
+     <button id="press" >BUY NOW</button>
+    `;
+    div.style.color = "white"
+    div.style.width = "30%"
+    
+    cars.appendChild(div);
+  })
   })
   .catch(error => console.error('Error fetching data:', error));
-  
+
+  const press = document.getElementById("press");
+  press.addEventListener("click", () => {
+    
+  })
+
+
+
